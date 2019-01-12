0b575b659666c085e2c04bec71a655c2ab50773b
mnao305 <naomasa305@gmail.com>
2019-01-13 00:09:56 +0900

    add:footer

---
+++ client/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer
+    app
+    class="pa-3">
+    &copy; tweetCategorized
+    <v-spacer />
+    <a
+      class="pr-3"
+      href="https://github.com/mnao305/tweetCategorized"
+      target="_blank">GitHub</a>
+    <a
+      href="https://twitter.com/mnao_305"
+      target="_blank">Twetter</a>
+  </v-footer>
+</template>
