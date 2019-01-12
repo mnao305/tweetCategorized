5250a1e72513e26947e4223b5f16eace3c7651f9
mnao305 <naomasa305@gmail.com>
2019-01-13 00:49:29 +0900

    add:header
    動きはまた今度

---
+++ client/components/TheHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-side-icon/>
+    <v-toolbar-title>tweetCategorized</v-toolbar-title>
+    <v-spacer />
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
