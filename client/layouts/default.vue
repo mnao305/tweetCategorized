ba13370f96d1412cafad44cdff21a91d0e738b29
mnao305 <naomasa305@gmail.com>
2019-01-13 05:07:15 +0900

    add:デフォルトレイアウト

---
+++ client/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <the-header/>
+    <TheCategorybar />
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <the-footer />
+  </v-app>
+</template>
+
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+import TheFooter from '~/components/TheFooter.vue'
+import TheCategorybar from '~/components/TheCategorybar.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+    TheCategorybar
+  }
+}
+</script>
