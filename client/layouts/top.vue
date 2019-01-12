bed21c532fc5ae65cb00dff9717d06a804914ed9
mnao305 <naomasa305@gmail.com>
2019-01-13 05:21:28 +0900

    add:トップページレイアウト

---
+++ client/layouts/top.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <the-header/>
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <the-footer />
+  </v-app>
+</template>
+
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+import TheFooter from '~/components/TheFooter.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
