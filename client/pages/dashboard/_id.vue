06b1f79cf6ed87c3ac0b62da3950fa34d7c48018
mnao305 <naomasa305@gmail.com>
2019-01-14 23:14:48 +0900

    update:ツイート一覧画面をそれっぽいデザインにした

--- client/pages/dashboard/_id.vue
+++ client/pages/dashboard/_id.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <div
-      v-for="tw in tweets"
-      :key="tw">
-      <Tweet :id="tw" />
+      v-masonry
+      transition-duration="0s"
+      item-selector=".item"
+      class="masonry-container">
+      <div
+        v-masonry-tile
+        v-for="(tw, index) in tweets"
+        :key="index"
+        class="item">
+        <Tweet :id="tw" />
+      </div>
     </div>
   </div>
 </template>
@@ -15,17 +23,40 @@ export default {
   components: {
     Tweet
   },
-  data() {
+  async asyncData() {
     return {
+      // テストデータ
       tweets: [
         '1084378745568354304',
         '1072787982413754368',
-        '1083332271292235776'
+        '1083332271292235776',
+        '1083320027539894272',
+        '1083602018294411266',
+        '1084367818198474752',
+        '1084363838907019266',
+        '1079307918401368064'
       ]
     }
   },
   mounted() {
-    // this.tweets = this.$axios.$get('http://127.0.0.1:8000/api/fav')
+    // 一回だけじゃ上手くいかないことがあったのでそれ用の対応
+    let loadFlag = 0
+    const intervalID = setInterval(() => {
+      if (loadFlag >= 10) {
+        clearInterval(intervalID)
+      }
+      console.log(loadFlag)
+
+      this.$redrawVueMasonry()
+      loadFlag++
+    }, 500)
   }
 }
 </script>
+
+<style>
+.item {
+  max-width: 250px;
+  padding: 0 5px;
+}
+</style>
