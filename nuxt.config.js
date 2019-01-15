e87c796a898618377acda89a435a63acbdc602b6
mnao305 <naomasa305@gmail.com>
2019-01-15 23:08:57 +0900

    add:これも無いと動かないっぽい

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,6 +55,15 @@ module.exports = {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID
+  },
+
   /*
   ** Build configuration
   */
