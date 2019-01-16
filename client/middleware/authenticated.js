52932da5871d8da7b0e0b0ebf085dc8a2da25b65
mnao305 <naomasa305@gmail.com>
2019-01-16 19:21:06 +0900

    update:Middlewareの認証処理をfirebaseのものに変更した
    こっちのほうが良さそう

--- client/middleware/authenticated.js
+++ client/middleware/authenticated.js
@@ -1,5 +1,17 @@
+import firebase from '~/plugins/firebase'
+
 export default function({ store, route, redirect }) {
-  if (!store.state.users.user && /^dashboard.*$/.test(route.name)) {
-    redirect('/')
-  }
+  firebase.auth().onAuthStateChanged(user => {
+    if (!user) {
+      // サインインしていない状態
+      if (/^dashboard.*$/.test(route.name)) {
+        redirect('/')
+      }
+      store.commit('users/logout')
+    } else {
+      // サインイン済み
+      const payload = firebase.auth().currentUser.providerData[0]
+      store.dispatch('users/setUser', payload)
+    }
+  })
 }
