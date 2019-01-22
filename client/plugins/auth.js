01bf37b569eee227c7c1827247c69e19ac2a2376
mnao305 <naomasa305@gmail.com>
2019-01-22 11:01:53 +0900

    add:authファイルに認証処理を追加
    ログイン・ログアウトはここを読み込む

--- client/plugins/auth.js
+++ client/plugins/auth.js
@@ -1,10 +1,35 @@
 import firebase from '~/plugins/firebase'
+import 'firebase/auth'
 
-function auth() {
-  return new Promise((resolve, reject) => {
-    firebase.auth().onAuthStateChanged(user => {
-      resolve(user || false)
+const provider = new firebase.auth.TwitterAuthProvider()
+
+const auth = {
+  // サインイン
+  login() {
+    return new Promise((resolve, reject) => {
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => resolve(result))
+        .catch(error => reject(error))
     })
-  })
+  },
+  // サインアウト
+  logout() {
+    return new Promise((resolve, reject) => {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => resolve())
+        .catch(error => reject(error))
+    })
+  },
+  // 認証状態の変更検知
+  auth() {
+    return new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(user => resolve(user))
+    })
+  }
 }
+
 export default auth
