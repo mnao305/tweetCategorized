42bb9f808d085c674e3a133bf59755e5ec030303
mnao305 <naomasa305@gmail.com>
2019-02-08 15:43:07 +0900

    Nuxt用に作っていたServerの削除

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.use('/twitter', require('./twitter'))
-router.use('/category', require('./category'))
-router.use('/user', require('./user'))
-
-module.exports = router
