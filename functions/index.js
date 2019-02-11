3bb64997e915d9f5bc54287aa84b0ad04bf53df7
mnao305 <naomasa305@gmail.com>
2019-02-11 19:48:12 +0900

    add: Twitterのいいね取得API

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors({ origin: true }))
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
-})
+app.use('/twitter/', require('./twitter'))
+
+exports.api = functions.https.onRequest(app)
---
+++ functions/twitter.js
@@ -0,0 +1,38 @@
+const Twit = require('twit')
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+admin.initializeApp()
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/getfav', (req, res) => {
+  if (
+    req.header('Authorization') &&
+    req.header('accessToken') &&
+    req.header('accessTokenSecret')
+  ) {
+    const idToken = req.header('Authorization').split(' ')[1]
+    admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then(decodedToken => {
+        const twitter = new Twit({
+          consumer_key: functions.config().twitter.apikey,
+          consumer_secret: functions.config().twitter.apisecret,
+          access_token: req.header('accessToken'),
+          access_token_secret: req.header('accessTokenSecret')
+        })
+        twitter.get('favorites/list', { count: 100 }, (_, data) => {
+          res.send(data)
+        })
+      })
+      .catch(() => {
+        res.send('auth Error')
+      })
+  } else {
+    res.send('non auth')
+  }
+})
+
+module.exports = router
