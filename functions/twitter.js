a37422633c9aefc3a4f9a5d5748b3fea99a19725
mnao305 <naomasa305@gmail.com>
2019-05-14 18:21:17 +0900

    add: TweetIDの存在チェックAPI

--- functions/twitter.js
+++ functions/twitter.js
@@ -45,4 +45,42 @@ router.get('/getfav', (req, res) => {
   }
 })
 
+router.get('/idcheck', (req, res) => {
+  if (
+    req.header('Authorization') &&
+    req.header('accessToken') &&
+    req.header('accessTokenSecret') &&
+    req.query.id
+  ) {
+    const idToken = req.header('Authorization').split(' ')[1]
+    admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then(async decodedToken => {
+        const twitter = new Twit({
+          consumer_key: functions.config().twitter.apikey,
+          consumer_secret: functions.config().twitter.apisecret,
+          access_token: req.header('accessToken'),
+          access_token_secret: req.header('accessTokenSecret')
+        })
+
+        let statusCheck
+        try {
+          await twitter.get('statuses/show', {
+            id: req.query.id
+          })
+          statusCheck = true
+        } catch (e) {
+          statusCheck = false
+        }
+        res.send(statusCheck)
+      })
+      .catch(() => {
+        res.send('auth Error')
+      })
+  } else {
+    res.send('non auth')
+  }
+})
+
 module.exports = router
